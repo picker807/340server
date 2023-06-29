a874d368d644f9f793d4ffd78c12f26e89ab1926
picker807 <60443210+picker807@users.noreply.github.com>
2023-06-29 13:47:39 -0400

    Added sanitation and validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,8 +2,8 @@
 const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
-//const errorController = require("../controllers/errorController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 // Route to account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -12,6 +12,9 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/registration", utilities.handleErrors(accountController.buildRegister));
 
 // Route to register new account
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post("/register",
+regValidate.registrationRules(),
+regValidate.checkRegData,
+utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
